e907428903393413e084e8f62c7bb027a4136deb
MorentDennis <dennis.morent@student.howest.be>
2018-10-03 16:43:13 +0200

    test fix

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ class BasicBot {
             // Perform a call to LUIS to retrieve results for the current activity message.
             const results = await this.luisRecognizer.recognize(context);
             const topIntent = LuisRecognizer.topIntent(results);
-            let entities = context.entities;
+            let accountLabel =  results.entities["Account"];
 
             // Determine what we should do based on the top intent from LUIS.
        
@@ -71,7 +71,7 @@ class BasicBot {
             switch (topIntent) {
             case CHECKACCOUNT_INTENT: 
            // let account = entities[0];
-            await context.sendActivity(`so you want to check the balance from ` + entities);
+            await context.sendActivity(`so you want to check the balance from ` + accountLabel);
             break;
             case GREETING_INTENT:
                 await context.sendActivity(`Hello.`);
