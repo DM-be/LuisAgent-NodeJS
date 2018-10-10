a447b52347282f8645385e5f5a066eb8b7ed8daa
MorentDennis <dennis.morent@student.howest.be>
2018-10-10 15:11:29 +0200

    cleanup custom getbudget prompt

--- src/shared/prompts/getCategoryNameWithABudgetPrompt.ts
+++ src/shared/prompts/getCategoryNameWithABudgetPrompt.ts
@@ -1,4 +1,6 @@
-import { EntityService } from './../helpers/entityService';
+import {
+    EntityService
+} from './../helpers/entityService';
 import {
     StatePropertyAccessor
 } from 'botbuilder';
@@ -11,54 +13,52 @@ import axios, {
     AxiosRequestConfig,
     AxiosPromise
 } from 'axios';
-import { OnTurnProperty } from '../stateProperties/onTurnProperty';
+import {
+    OnTurnProperty
+} from '../stateProperties/onTurnProperty';
 
 
 export class GetCategoryNameWithABudgetPrompt extends TextPrompt {
 
     /**
      * custom prompt for getting a categoryName, with a budget, and validating it against known categories containing a tracked budget
+     * overrides the continueDialog to check for cancel,... intents
      * @param {String []} categoryNamesWithABudget variable holding categoryNames for the validator, gets filled on constructing via a promise
      */
 
-    
     constructor(private dialogId: string, private botConfig: any, private entityService: EntityService, private onTurnAccessor: StatePropertyAccessor) {
         super(dialogId, async (prompt: PromptValidatorContext < string > ) => {
-             
             const value = prompt.recognized.value.toLowerCase();
-            if (entityService.getCategoryNamesWithABudget().findIndex(acc => acc === value) === -1) {
+            if (!entityService.categoryNamesWithABudgetContains(value)) {
                 await prompt.context.sendActivity(`You dont have a category named ${value} with a tracked budget, please provide correct one`);
                 return false;
             }
-            
             return true;
         });
         if (!dialogId) throw new Error('Need dialog ID');
         if (!botConfig) throw new Error('Need bot configuration');
+        if (!onTurnAccessor) throw new Error('Need onturnaccessor!');
     }
- 
-     /**
-         * Override dialogContinue.
-         *   The override enables
-         *     recognizing the cancel intent to cancel the dialog
-         *
-         * @param {DialogContext} dc context
-         */
-        async continueDialog(dc: DialogContext) {
-            let turnContext = dc.context;
-           // let step = dc.activeDialog.state;
-            const onTurnProperty: OnTurnProperty = await this.onTurnAccessor.get(turnContext);
-            switch (onTurnProperty.getIntent()) {
-                case 'Cancel':
+    /**
+     * Override continueDialog.
+     *   The override enables
+     *     recognizing the cancel intent to cancel the dialog
+     *     todo: recognizing other intents and resolving them appropiatly
+     *     ...
+     * @param {DialogContext} dc context
+     */
+    async continueDialog(dc: DialogContext) {
+        let turnContext = dc.context;
+        // let step = dc.activeDialog.state;
+        const onTurnProperty: OnTurnProperty = await this.onTurnAccessor.get(turnContext);
+        switch (onTurnProperty.getIntent()) {
+            case 'Cancel':
                 await dc.context.sendActivity('ok ill cancel this conversation for you :)');
                 return await dc.cancelAllDialogs();
-                case 'None':
+            case 'None':
                 return await super.continueDialog(dc);
-                default:
+            default:
                 return await super.continueDialog(dc);
-            }
-
-
-
         }
+    }
 }
